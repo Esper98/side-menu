5902fd2f50ff86b32daaa9c3c2a67b7aa266b873
esper van den heuvel <espervdheuvel@hotmail.com>
2018-12-04 22:44:11 +0100

    added icon animation

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ let menuIcon = document.querySelector("#menu-icon");
 let menu = document.querySelector("#menu-list");
 
 menuIcon.addEventListener("click", fadeMenu);
-menu.style.transform = "translateX(300px)";
+menu.style.transform = "translateX(500px)";
 
 function fadeMenu(){
-
+    menuIcon.classList.toggle("change");
     console.log("test");
-    if (menu.style.transform === "translateX(300px)")
+    var width = menu.width;
+    console.log(menu.style);
+    if (menu.style.transform === "translateX(500px)")
     {
         menu.style.transform = "translateX(0px)";
 
     } else{
-        menu.style.transform = "translateX(300px)";
+        menu.style.transform = "translateX(500px)";
     }
 }
